6f77d7603b7b8628da2423b012a4a69cb78893f7
faridullahfrotan <faridullahfrotan0@gmail.com>
2025-12-22 20:00:30 +0430

    add project files

---
+++ js/app.js
@@ -0,0 +1,61 @@
+const hourHand = document.querySelector('.hour');
+const minuteHand = document.querySelector('.minute');
+const secondHand = document.querySelector('.second');
+const hourBox = document.querySelector('.hourBox')
+const minuteBox = document.querySelector('.minuteBox')
+const secondBox = document.querySelector('.secondBox')
+let span = document.querySelector('.span')
+const time = new Date()
+let totalSeconds = time.getSeconds()
+
+let period
+function updateClock() {
+    const now = new Date();
+
+    let seconds = now.getSeconds();
+    let minutes = now.getMinutes();
+    let hours = now.getHours();
+
+    period = "Pm"
+    hours = hours % 12
+    if (hours === 0) {
+        hours = 12
+        period = 'Am'
+    }
+
+    totalSeconds = totalSeconds + 1
+    let secondDeg = (totalSeconds * 6) - 96
+
+    let minuteDeg = (minutes * 6) - 90
+
+    let hoursDeg = ((hours + ((minutes) / 60)) * 30) - 90
+
+    console.log(hoursDeg)
+    secondHand.style.transform = 'rotate(' + secondDeg + 'deg)'
+    minuteHand.style.transform = 'rotate(' + minuteDeg + 'deg)'
+    hourHand.style.transform = 'rotate(' + hoursDeg + 'deg)'
+
+    span.innerHTML = period
+
+    if (seconds < 10) {
+        secondBox.innerHTML = "0" + seconds
+    } else {
+        secondBox.innerHTML = seconds
+    }
+
+    if (hours < 10) {
+        hourBox.innerHTML = "0" + hours
+    } else {
+        hourBox.innerHTML = hours
+    }
+
+    if (minutes < 10) {
+        minuteBox.innerHTML = "0" + minutes
+    } else {
+        minuteBox.innerHTML = minutes
+    }
+
+}
+
+updateClock();
+setInterval(updateClock, 1000);
---
+++ readme.md
@@ -0,0 +1,52 @@
+# 🕒 Analog & Digital Clock UI
+
+A modern and visually appealing *Analog + Digital Clock* built with *HTML, CSS, and JavaScript*.  
+This project features a smooth analog clock, a digital time display, and a day/night theme inspired by real-world lighting.
+
+---
+
+## ✨ Features
+
+- ⏰ *Analog Clock*
+  - Smooth hour, minute, and second hands
+  - Clean circular dial with numbers
+- 🖥️ *Digital Clock*
+  - Displays hours, minutes, and seconds
+  - AM / PM indicator
+- 🌙 *Day & Night Theme*
+  - Sun/Moon visual indicator
+  - Dark gradient background for night mode
+- 🎨 *Modern UI Design*
+  - Gradient background
+  - Minimal and clean layout
+- ⚡ *Real-time Updates*
+  - Uses JavaScript Date() API
+  - Updates every second
+
+---
+
+## 🛠️ Built With
+
+- *HTML5*
+- *CSS3*
+  - Flexbox
+  - Gradients
+  - Transforms & transitions
+- *Vanilla JavaScript*
+  - Date
+  - setInterval
+  - DOM manipulation
+
+---
+
+## 📸 Preview
+
+![Clock Preview](./Screenshot%202025-12-21%20233356.png)
+
+---
+
+## 🚀 Getting Started
+
+### 1. Clone the repository
+```bash
+git clone https://github.com/faridullahfrotan/analog-digital-clock.git
